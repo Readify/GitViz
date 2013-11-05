f79f12a19502aba0c4a220023d12421a4862c610
Tatham Oddie <tatham@oddie.com.au>
2013-11-05 16:26:39 +1100

    Refactor to use a view model

---
+++ Logic/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+
+namespace GitViz.Logic
+{
+    public class ViewModel
+    {
+        string _repositoryPath;
+        LogRetriever _logRetriever;
+
+        readonly LogParser _parser = new LogParser();
+
+        public ViewModel()
+        {
+            RepositoryPath = @"c:\temp\git-bash-test";
+        }
+
+        public string RepositoryPath
+        {
+            get { return _repositoryPath; }
+            set
+            {
+                _repositoryPath = value;
+                if (IsValidGitRepository(_repositoryPath))
+                {
+                    var commandExecutor = new GitCommandExecutor(_repositoryPath);
+                    _logRetriever = new LogRetriever(commandExecutor, _parser);
+
+                    _commits.Clear();
+                    var commits =_logRetriever.GetLog().ToList();
+                    commits.ForEach(_commits.Add);
+                }
+                else
+                {
+                    _logRetriever = null;
+                    _commits.Clear();
+                }
+            }
+        }
+
+        readonly ObservableCollection<Commit> _commits = new ObservableCollection<Commit>();
+        public ObservableCollection<Commit> Commits
+        {
+            get { return _commits; }
+        }
+
+        static bool IsValidGitRepository(string path)
+        {
+            return Directory.Exists(path)
+                && Directory.Exists(Path.Combine(path, ".git"));
+        }
+    }
+}
--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -1,56 +1,10 @@
-﻿using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using GitViz.Logic;
-
-namespace UI
+﻿namespace UI
 {
     public partial class MainWindow
     {
-        string _repositoryPath;
-        LogRetriever _logRetriever;
-
-        readonly LogParser _parser = new LogParser();
-
         public MainWindow()
         {
-            RepositoryPath = @"c:\temp\git-bash-test";
             InitializeComponent();
-        }
-
-        public string RepositoryPath
-        {
-            get { return _repositoryPath; }
-            set
-            {
-                _repositoryPath = value;
-                if (IsValidGitRepository(_repositoryPath))
-                {
-                    var commandExecutor = new GitCommandExecutor(_repositoryPath);
-                    _logRetriever = new LogRetriever(commandExecutor, _parser);
-
-                    _commits.Clear();
-                    var commits =_logRetriever.GetLog().ToList();
-                    commits.ForEach(_commits.Add);
-                }
-                else
-                {
-                    _logRetriever = null;
-                    _commits.Clear();
-                }
-            }
-        }
-
-        readonly ObservableCollection<Commit> _commits = new ObservableCollection<Commit>();
-        public ObservableCollection<Commit> Commits
-        {
-            get { return _commits; }
-        }
-
-        static bool IsValidGitRepository(string path)
-        {
-            return Directory.Exists(path)
-                && Directory.Exists(Path.Combine(path, ".git"));
         }
     }
 }
